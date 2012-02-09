f0c3bc7e46d86d2bdf2cdfb8533893fcd065d2c3
Simon <simon@isengard.biz>
2012-02-09 09:10:03 +0000

    Tweak Yubikey login page and improve comments.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/yubikey/trunk@23442 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- token.php
+++ token.php
@@ -31,19 +31,19 @@ $valid_op = array('login', '');
 // Check that op is a permitted (whitelisted) value
 if (in_array($dirty_op, $valid_op, TRUE))
 {
-	// We accept that op is now clean and safe to use
+	// We accept that op is a whitelisted value that is safe to use
 	$clean_op = $dirty_op;
 	
 	switch($clean_op)
 	{
 		case "login":
 
-			// initialise
+			// Initialise
 			$tokenObjects = array();
 			$otp_authenticated = $user_details = FALSE;
 			$clean_otp = $dirty_otp = $public_id = $criteria = $tokenObj = $user_details = $user_name = '';
 
-			// sanitise and validate one time password (alphanumeric string 44 characters in length)
+			// Sanitise and validate one time password (alphanumeric string 44 characters in length)
 			$myts =& MyTextsanitizer::getInstance();
 			$dirty_otp = isset($_POST['yubikey_otp']) ? strip_tags(trim($_POST['yubikey_otp'])) : '';
 			$dirty_otp = mysql_real_escape_string($myts->stripSlashesGPC($dirty_otp));
@@ -57,54 +57,55 @@ if (in_array($dirty_op, $valid_op, TRUE))
 			// it is cleaned & handled by the included checklogin.php. Just make sure it is present.
 			if ($_POST['pass'] && !empty($clean_otp))
 			{
-				// extract the public_id from the Yubikey OTP
+				// Extract the public_id from the Yubikey OTP
 				$public_id = substr($clean_otp, 0, 12);
 				
-				// check the key exists in the database and look up the associated user
-				$criteria = icms_buildCriteria(array('public_id' => $public_id,
-						'yubikey_enabled' => TRUE));
+				// Check the key exists in the database and look up the associated user
+				$criteria = icms_buildCriteria(array('public_id' => $public_id));
 				$tokenObjects = $yubikey_token_handler->getObjects($criteria);
 				if (!empty($tokenObjects))
 				{
 					$tokenObj = array_shift($tokenObjects);
 					
-					// validate the ICMS account password against the associated user
-					// need to use the userid to lookup the username
-					$member_handler = icms::handler("icms_member");
-					$user_details = $member_handler->getUser($tokenObj->getVar('user_id', 'e'));
-					
-					if ($user_details)
+					// Check the key is actually enabled
+					if ($tokenObj->getVar('yubikey_enabled') == 1)
 					{
-						// validate the one-time Yubikey password against Yubico's authentication server
-						$otp_authenticated = $tokenObj->verify($clean_otp);
-						if ($otp_authenticated)
+						// Lookup the associated user
+						$member_handler = icms::handler("icms_member");
+						$user_details = $member_handler->getUser($tokenObj->uid());
+
+						if ($user_details)
 						{
-							
-							// Set flag indicating checklogin.php  is running from Yubikey page.
-							// 
-							// This prevents a section of code running in checklogin.php, 
-							// which should only operate from the standard ICMS login page/block. 
-							// The code checks if a user account requires Yubikey authentication and 
-							// redirects them to the Yubikey login page where appropriate.
-							
-							$yubikey_login_page = TRUE;
-							
-							// One time password validated, now procede to validate ICMS password. 
-							// 
-							// NB: ICMS allows both the login_name and email address of a user to be
-							// used as the login. Old user accounts may not have a login_name, as 
-							// this feature was added recently. Therefore it is safer to use the 
-							// email address as the login name. The login name needs to be put back 
-							// into $_POST as the yubikey login page does not ask for it, but rather
-							// uses the public ID embedded in the Yubikey one time password to 
-							// lookup the associated user account and get the name. Putting it 
-							// into the $_POST allows the standard checklogin.php script to be used
-							// with minimal modification.
-							
-							$login_name = $user_details->getVar('email', 'e');
-							$_POST['uname'] = $login_name;
-							unset($user_details);
-							include ICMS_ROOT_PATH . '/include/checklogin.php';
+							// Validate the one-time Yubikey password against Yubico's authentication server
+							$otp_authenticated = $tokenObj->verify($clean_otp);
+							if ($otp_authenticated)
+							{
+								// Set flag indicating checklogin.php is running from Yubikey page.
+								// 
+								// This prevents a section of code running in checklogin.php, 
+								// which should only operate from the standard ICMS login page/block. 
+								// The code checks if a user account requires Yubikey authentication and 
+								// redirects them to the Yubikey login page if appropriate.
+
+								$yubikey_login_page = TRUE;
+
+								// One time password validated, now procede to validate ICMS password. 
+								// 
+								// NB: ICMS allows both the login_name and email address of a user to be
+								// used as the login. Old user accounts may not have a login_name, as 
+								// this feature was added recently. Therefore it is safer to use the 
+								// email address as the login name. The login name needs to be put back 
+								// into $_POST as the yubikey login page does not ask for it, but rather
+								// uses the public ID embedded in the Yubikey one time password to 
+								// lookup the associated user account and get the name. Putting it 
+								// into the $_POST allows the standard checklogin.php script to be used
+								// with minimal modification.
+
+								$login_name = $user_details->getVar('email', 'e');
+								$_POST['uname'] = $login_name;
+								unset($user_details);
+								include ICMS_ROOT_PATH . '/include/checklogin.php';
+							}
 						}
 					}
 				}				
@@ -115,13 +116,12 @@ if (in_array($dirty_op, $valid_op, TRUE))
 			}
 			
 			// Otherwise / if any check fails, redirect back to login page
-			global $yubikeyModule;
 			redirect_header(ICMS_URL . '/modules/' . basename(dirname(__FILE__))
 						. '/token.php', 2, _CO_YUBIKEY_LOGIN_FAILED);
 
 			break;
 
-		default: // display empty login form
+		default: // Display empty login form
 	}
 }
 
