7d387625c3357de7e67e85349b6cb34b3fbbdc1f
Simon <simon@isengard.biz>
2012-06-16 01:59:30 +0000

    Mark as V1.12 final.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/yubikey/trunk@24156 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- icms_version.php
+++ icms_version.php
@@ -17,7 +17,7 @@ if (!defined("ICMS_ROOT_PATH")) die("ICMS root path not defined");
 /**  General Information  */
 $modversion = array(
   'name'=> _MI_YUBIKEY_MD_NAME,
-  'version'=> 1.11,
+  'version'=> 1.12,
   'description'=> _MI_YUBIKEY_MD_DESC,
   'author'=> "Madfish (Simon Wilkinson)",
   'credits'=> "Tom Corwine developed the Yubikey PHP API calls used in this module.",
@@ -32,9 +32,9 @@ $modversion = array(
   'image'=> "images/icon_big.png", /* for backward compatibility */
 
 /**  Development information */
-  'status_version'=> "1.11",
+  'status_version'=> "1.12",
   'status'=> "Final",
-  'date'=> "9/6/2012",
+  'date'=> "16/6/2012",
   'author_word'=> "This module is not a substitute for sensible password management. However, it protects designated accounts in the events their password is compromised.",
 
 /** Contributors */
